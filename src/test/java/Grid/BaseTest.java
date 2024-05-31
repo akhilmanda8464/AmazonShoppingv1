0a2a09bd1e6ed4c1a4019a9f857de1bf7d224016
akhilmanda8464 <131892338+akhilmanda8464@users.noreply.github.com>
2024-05-31 12:47:01 +0530

    Update BaseTest.java

--- src/test/java/Grid/BaseTest.java
+++ src/test/java/Grid/BaseTest.java
@@ -40,9 +40,9 @@ public class BaseTest {
         driver.set(new RemoteWebDriver(new URL(remote_url), capabilities));
         driver.get().get("https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0");
         driver.get().manage().window().maximize();
-        driver.get().manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
-        WebDriverWait wait = new WebDriverWait(driver.get(), Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"ap_email\"]")));
+        //driver.get().manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        //WebDriverWait wait = new WebDriverWait(driver.get(), Duration.ofSeconds(10));
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"ap_email\"]")));
     }
  
     public WebDriver getDriver() {
