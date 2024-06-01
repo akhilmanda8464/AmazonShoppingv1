bc7d10cfb894656474d34f7e670fdca1f36c8a06
akhilmanda8464 <131892338+akhilmanda8464@users.noreply.github.com>
2024-06-01 16:43:12 +0530

    Update BaseTest.java

--- src/test/java/Grid/BaseTest.java
+++ src/test/java/Grid/BaseTest.java
@@ -33,9 +33,9 @@ public class BaseTest {
             capabilities = new FirefoxOptions();
         } else if (browser.equals("chrome")) {
             capabilities = new ChromeOptions();
-        } else if (browser.equals("edge")) {
-            capabilities = new EdgeOptions();
-        }
+        }// else if (browser.equals("edge")) {
+           // capabilities = new EdgeOptions();
+       //}
  
         driver.set(new RemoteWebDriver(new URL(remote_url), capabilities));
         driver.get().get("https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0");
